8e989a10ed62736652c4f51b9d33e3d46775d438
Alexander Yordanov <a.yordanow67@gmail.com>
2024-06-14 00:03:27 +0300

    Fixed deployment issues

--- app/_components/NavButton.js
+++ app/_components/NavButton.js
@@ -1,7 +1,3 @@
-import { IoMdClose } from "react-icons/io";
-
-import { RxHamburgerMenu } from "react-icons/rx";
-
 function NavButton({ onClick, navBarOpen }) {
   return (
     <button
@@ -9,9 +5,35 @@ function NavButton({ onClick, navBarOpen }) {
       className="z-20 absolute top-12 right-6 text-primary-100 "
     >
       {navBarOpen ? (
-        <IoMdClose className="w-6 h-6" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
       ) : (
-        <RxHamburgerMenu className="w-6 h-6" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          />
+        </svg>
       )}
     </button>
   );
