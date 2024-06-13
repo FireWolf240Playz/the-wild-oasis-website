968d951d87190776cbf36ecb961ed60ef9d783d8
Alexander Yordanov <a.yordanow67@gmail.com>
2024-06-14 00:04:25 +0300

    fixed small bug

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -4,7 +4,7 @@ import Logo from "@/app/_components/Logo";
 function Header() {
   return (
     <header className="border-b border-primary-900 px-8 py-5 max-400:fle ">
-      <div className="flex items-center justify-around">
+      <div className="flex items-center justify-between">
         <Logo />
         <Navigation />
       </div>
